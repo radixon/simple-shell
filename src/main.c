7ed6c12b956968b0083f5b016c85c281782bdbdc
radixon <radixon28@yahoo.com>
2026-03-10 23:02:19 -0400

    Parse commands and arguments

--- src/main.c
+++ src/main.c
@@ -6,11 +6,16 @@
 
 
 int main(void){
-    const int kb = 1024;
-    char cwd[kb];     // Buffer to store current working directory
-    char command[kb]; // Buffer to store user input
+    const int KB = 1024;
+    const int MAX_ARGS = 64;    // Maximum number of arguments for a command
+
+
+    char cwd[KB];     // Buffer to store current working directory
+    char command[KB]; // Buffer to store user input
+    char *args[MAX_ARGS + 1];   // Add 1 for NULL terminator required by execve
 
     while(true){
+        /*******  Display the prompt  ******/
         // getcwd returns NULL on error, otherwise getcwd returns a point to the buffer cwd[1024]
         if(getcwd(cwd, sizeof(cwd)) != NULL){
             printf("%s>", cwd);
@@ -21,22 +26,53 @@ int main(void){
         }
 
 
-        // read one line from standard input
+        /*******  Read command from user  ******/
         if(fgets(command, sizeof(command), stdin) != NULL){
             command[strcspn(command, "\n")] = 0;    // strcspn replaces first newline with null terminator due to fgets including newline character
             printf("Command entered: '%s'\n", command); // Print command to confirm the command was read correctly
         }
         else{
-            printf("\nNo input received. Exiting.\n");
-        }
-
-        // check for exit command
-        if(strcmp(command, "exit") == 0){
-            printf("---Exiting shell---\n");
+            printf("\nExiting Shell.\n");
             break;
         }
-        else if(strcmp(command, "exit") > 0){
-            printf("---Command Received: %s---\n", command);
+
+        /*  Remove Trailing newline character*/
+        command[strcspn(command, "\n")] = 0;
+
+        /*  --exit-- built-in command */
+        if(strcmp(command, "exit") == 0){
+            printf("Exiting Shell.\n");
+            break;
+        }
+
+        /*  Parse command and arguments */
+        if(strlen(command) == 0){ // --Enter-- pressed by user
+            continue;
+        }
+
+        char *token;
+        int i = 0;
+
+        /*  Get first token  */
+        token = strtok(command, " ");
+        while(token != NULL && i < MAX_ARGS){
+            args[i] = token;
+            i++;
+            // Get next token
+            token = strtok(NULL, " ");
+        }
+
+        args[i] = NULL;     // exec functions require a NULL-terminated array of arguments
+
+        /*  Print parsed command and arguments  */
+        if(args[0] != NULL){
+            printf("Commnad: '%s'\n", args[0]);
+            for(int j=1; args[j] != NULL; j++){
+                printf("Argument %d: '%s'\n", j, args[j]);
+            }
+        }
+        else{
+            printf("No command parsed.\n");
         }
     }
     
